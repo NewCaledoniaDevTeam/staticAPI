02550b52fb20377fabacc833c8ce26870f44ac0a
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-01-30 12:38:40 -0600

    Update compress-json.sh

--- compress-json.sh
+++ compress-json.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 for file in ./**/*index.json; do
-    mkdir "$(dirname $(readlink -f "$file"))/min"
+    jq -c < "$file" > "${file%.*}.min.json"
+done
+
+for file in ./**/**/*index.json; do
     jq -c < "$file" > "${file%.*}.min.json"
 done
